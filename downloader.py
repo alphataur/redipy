774c8b7b64dfe23b99ef4009f3f86911a462e7d2
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-07-01 17:35:23 +0530

    minor updates to downloader

--- downloader.py
+++ downloader.py
@@ -11,9 +11,10 @@ class Downloader:
         self.pool_size = pool_size
         self.running = False
         self.session = False
-        self.close = False
+        self._close = False
         self.success = []
         self.failed = []
+        self.session = aiohttp.ClientSession()
 
     def close(self, force=False):
         #gracefull exit
@@ -39,15 +40,11 @@ class Downloader:
                     "error": False,
                     "success": True
                 }
-    async def start_session(self):
-        self.session = await aiohttp.ClientSession()
 
     def add(self, uri, fpath):
         self.queue.put_nowait((uri, fpath))
 
     async def download(self, uri, fpath, retries=5):
-        if not self.session:
-            await self.start_session()
         try:
             async with self.session.get(uri) as resp:
                 if resp.status == 200:
@@ -62,13 +59,18 @@ class Downloader:
                 return self.errorify(uri, fpath, error)
 
     async def worker(self):
-        while not self.close:
-            uri, fpath = await self.queue.get()
-            result = await self.download(uri, fpath)
-            if result["success"]:
-                self.success.append(result)
-            else:
-                self.failed.append(result)
+        while not self._close:
+            try:
+                task = self.queue.get()
+                uri, fpath  = await asyncio.wait_for(task, timeout=10)
+                result = await self.download(uri, fpath)
+                print(result)
+            except asyncio.TimeoutError as te:
+                print("worker is idle for too long... exiting", te)
+                break
+            except Exception as e:
+                print("worker failed at", e)
+                continue
 
     def consume(self, name):
         files = glob.glob(name)
@@ -79,9 +81,9 @@ class Downloader:
                     self.add(datum["url"], datum["fpath"])
 
     def start(self):
-        self.workers = asyncio.gather(*[asyncio.ensure_future(self.worker()) for i in range(self.pool_size)])
+        self.workers = asyncio.gather(*[self.worker() for i in range(self.pool_size)])
         self.loop.run_until_complete(self.workers)
 
 handle = Downloader(pool_size=10)
 handle.consume("eggs/dakini/*.json")
-handle.download()
+handle.start()
